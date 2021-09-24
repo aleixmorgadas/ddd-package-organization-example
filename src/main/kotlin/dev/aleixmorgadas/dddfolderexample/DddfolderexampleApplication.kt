8bcdd4a45dab517159098d8fbb8e3fbb9c9de168
Aleix Morgadas <aleixmorgadas@users.noreply.github.com>
2021-09-24 08:51:59 +0200

    init

---
+++ src/main/kotlin/dev/aleixmorgadas/dddfolderexample/DddfolderexampleApplication.kt
@@ -0,0 +1,11 @@
+package dev.aleixmorgadas.dddfolderexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DddfolderexampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<DddfolderexampleApplication>(*args)
+}
---
+++ src/test/kotlin/dev/aleixmorgadas/dddfolderexample/DddfolderexampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.aleixmorgadas.dddfolderexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DddfolderexampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
